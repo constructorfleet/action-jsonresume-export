9d31dfbb10e8aa2448289aca41014d76c045b798
Teagan42 <teagan.glenn@resideo.com>
2023-02-08 20:04:42 -0700

    Fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,26 @@ RESUME=${2}
 OUTPUT=${3}
 FORMAT="${4:-html}"
 
-if [[ -d $THEME ]]; then
-    echo "$THEME is a directory";
-    if [[ -f "$THEME/package.json" ]]; then
-      BASE_DIR=$(dirname -- "$( readlink -f -- "$0"; )");
-      echo "Installing theme dependencies";
-      cd $THEME;
-      npm install;
-      cd $BASE_DIR;
+if [ -d "$THEME" ]
+then
+    echo "$THEME is a directory"
+    if [ -f "$THEME/package.json" ]
+    then
+      BASE_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+      echo "Installing theme dependencies"
+      cd "$THEME"
+      npm install
+      cd "$BASE_DIR"
     fi
-elif [[ -f $THEME ]]; then
+elif [ -f "$THEME" ]
+then
     echo "$THEME is a file";
 else
-    THEME_PACKAGE=jsonresume-theme-${THEME};
-    echo "Installing theme: ${THEME}";
+    THEME_PACKAGE=jsonresume-theme-${THEME}
+    echo "Installing theme: ${THEME}"
 
     # NOTE: this needs to be installed locally, not globally
-    npm install ${THEME_PACKAGE};
+    npm install "${THEME_PACKAGE}"
 fi
 
-resume export --resume ${RESUME} --theme ${THEME} --format "${FORMAT}" ${OUTPUT}
+resume export --resume "${RESUME}" --theme "${THEME}" --format "${FORMAT}" "${OUTPUT}"
